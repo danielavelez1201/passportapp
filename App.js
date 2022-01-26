c385180388a2c83639326d0d4d6f367cfce9f89a
Jerry Ye <jerry1ye10@gmail.com>
2022-01-26 17:20:50 -0500

    add working &styled camera view

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { NativeBaseProvider, StatusBar } from "native-base";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import cambutton from "./cambutton.png";
+import { Camera } from "expo-camera";
+
+// export default function App() {
+//   return (
+//     <View style={style.container}>
+//       <CameraScan />
+//     </View>
+//   );
+// }
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      console.log("Hello World");
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={console.log("Yay!")}
+            style={{
+              height: 80,
+              width: 80,
+              resizeMode: "contain",
+              marginTop: "auto",
+              justifyContent: "center",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            <img src={cambutton} />
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -15,8 +56,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
   },
 });
---
+++ Scan.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+function ScanCamera() {
+  useEffect(async () => {
+    console.log("rendering");
+  });
+
+  return <Text>Hello World!!! </Text>;
+}
+
+export default ScanCamera;
